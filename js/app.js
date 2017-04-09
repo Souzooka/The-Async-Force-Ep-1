b0833f4e0aa9956fb740a3799a87e2381f38c3c1
Dakota <bazookaharwell@gmail.com>
2017-04-08 17:00:35 -1000

    make getData helper function

--- js/app.js
+++ js/app.js
@@ -1,43 +1,18 @@
-function person4NameHTML() {
-  const data = JSON.parse(this.responseText);
-  document.querySelector('#person4Name').innerHTML = data.name;
-}
+function getData(source, property, element) {
+  const oReq = new XMLHttpRequest();
 
-function person4HomeWorldHTML() {
-  const data = JSON.parse(this.responseText);
-  document.querySelector('#person4HomeWorld').innerHTML = data.name;
-}
+  oReq.addEventListener('load', function() {
+    const data = JSON.parse(this.responseText);
+    document.querySelector(element).innerHTML = data[property];
+  });
+  oReq.open('GET', source);
+  oReq.send();
 
-function person14NameHTML() {
-  const data = JSON.parse(this.responseText);
-  document.querySelector('#person14Name').innerHTML = data.name;
-}
-
-function person14SpeciesHTML() {
-  const data = JSON.parse(this.responseText);
-  document.querySelector('#person14Species').innerHTML = data.name;
 }
 
 const person4Name = new XMLHttpRequest();
 
-person4Name.addEventListener('load', person4NameHTML);
-person4Name.open('GET', 'http://swapi.co/api/people/4/');
-person4Name.send();
-
-const person4HomeWorld = new XMLHttpRequest();
-
-person4HomeWorld.addEventListener('load', person4HomeWorldHTML);
-person4HomeWorld.open('GET', 'http://swapi.co/api/planets/1/');
-person4HomeWorld.send();
-
-const person14Name = new XMLHttpRequest();
-
-person14Name.addEventListener('load', person14NameHTML);
-person14Name.open('GET', 'http://swapi.co/api/people/14/');
-person14Name.send();
-
-const person14Species = new XMLHttpRequest();
-
-person14Species.addEventListener('load', person14SpeciesHTML);
-person14Species.open('GET', 'http://swapi.co/api/species/1');
-person14Species.send();
+getData('http://swapi.co/api/people/4/', 'name', '#person4Name');
+getData('http://swapi.co/api/planets/1/', 'name', '#person4HomeWorld');
+getData('http://swapi.co/api/people/14/', 'name', '#person14Name');
+getData('http://swapi.co/api/species/1/', 'name', '#person14Species');
