2ebb0ae0a72ac1b5c48e6413dda220e21ecca8ef
Dakota <bazookaharwell@gmail.com>
2017-04-08 16:41:19 -1000

    get Han's info

--- js/app.js
+++ js/app.js
@@ -8,6 +8,16 @@ function person4HomeWorldHTML() {
   document.querySelector('#person4HomeWorld').innerHTML = data.name;
 }
 
+function person14NameHTML() {
+  const data = JSON.parse(this.responseText);
+  document.querySelector('#person14Name').innerHTML = data.name;
+}
+
+function person14SpeciesHTML() {
+  const data = JSON.parse(this.responseText);
+  document.querySelector('#person14Species').innerHTML = data.name;
+}
+
 const person4Name = new XMLHttpRequest();
 
 person4Name.addEventListener('load', person4NameHTML);
@@ -19,3 +29,15 @@ const person4HomeWorld = new XMLHttpRequest();
 person4HomeWorld.addEventListener('load', person4HomeWorldHTML);
 person4HomeWorld.open('GET', 'http://swapi.co/api/planets/1/');
 person4HomeWorld.send();
+
+const person14Name = new XMLHttpRequest();
+
+person14Name.addEventListener('load', person14NameHTML);
+person14Name.open('GET', 'http://swapi.co/api/people/14/');
+person14Name.send();
+
+const person14Species = new XMLHttpRequest();
+
+person14Species.addEventListener('load', person14SpeciesHTML);
+person14Species.open('GET', 'http://swapi.co/api/species/1');
+person14Species.send();
